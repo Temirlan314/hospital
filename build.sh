2c6590e8a4ce203f58d5cff7ac7d021854c804f8
temirlanprimtayev <temirlanprimtayev@gmail.com>
2022-11-21 10:07:07 +0600

    set up

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+poetry install
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+python manage.py createsuperuser --username admin
+
+pip install setuptools
