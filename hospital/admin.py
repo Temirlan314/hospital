166c8e34262128132e452a1a27f763366280e26e
temirlanprimtayev <temirlanprimtayev@gmail.com>
2022-11-21 15:57:24 +0600

    migrations fix

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import DiseaseType, Disease, Country, Discover, User, PublicServant, Record, Specialize, Doctor
+from .models import DiseaseType, Disease, Country, Discover, Users, PublicServant, Record, Specialize, Doctor
 
 # Register your models here.
 
