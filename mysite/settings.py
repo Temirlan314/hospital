ef84f4205ca7a3b214746773b90947ce86b4915c
temirlanprimtayev <temirlanprimtayev@gmail.com>
2022-11-21 16:51:17 +0600

    url change

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://temirlan:DE4eXWfyDOjNoqo5JF2WjDrD8sTs7QlC@dpg-cdtfkbo2i3mrfoh2itvg-a/hospital_nzbs')
+    'default': dj_database_url.config(default='postgres://temirlan:DE4eXWfyDOjNoqo5JF2WjDrD8sTs7QlC@dpg-cdtfkbo2i3mrfoh2itvg-a.oregon-postgres.render.com/hospital_nzbs')
 }
 
 
