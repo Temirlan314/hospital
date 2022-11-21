0c480c614b3f0e23c33004782799e27d949cf2e4
temirlanprimtayev <temirlanprimtayev@gmail.com>
2022-11-21 17:17:55 +0600

    url change to internal

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ poetry install
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser --username admin
+#python manage.py createsuperuser --username admin
 
 pip install setuptools
